499181a5863c70fc6f2781c192d666c5f8bc48ad
mrtenkorang <josephoforitenkorang390@gmail.com>
2025-12-01 14:04:22 +0000

    m

---
+++ lib/controller/cache/cache_service.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart' show debugPrint;
+import 'package:human_rights_monitor/controller/models/auth/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StaffCache {
+  static const String _staffKey = 'cached_staff_data';
+
+  // Save staff data to cache
+  static Future<void> saveStaff(UserModel staff) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final staffJson = staff.toJson();
+      final staffString = json.encode(staffJson);
+      await prefs.setString(_staffKey, staffString);
+    } catch (e) {
+      debugPrint('Error saving staff to cache: $e');
+    }
+  }
+
+  // Get staff data from cache
+  static Future<UserModel?> getStaff() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final staffString = prefs.getString(_staffKey);
+
+      if (staffString == null) {
+        return null;
+      }
+
+      final staffJson = json.decode(staffString) as Map<String, dynamic>;
+
+      // Parse assigned_districts
+      final assignedDistrictsJson = staffJson['assigned_districts'] as List;
+      final assignedDistricts = assignedDistrictsJson
+          .map((districtJson) => AssignedDistrict.fromJson(districtJson))
+          .toList();
+
+      return UserModel(
+        id: staffJson['id'] as int,
+        firstName: staffJson['first_name'] as String,
+        lastName: staffJson['last_name'] as String,
+        gender: staffJson['gender'] as String,
+        contact: staffJson['contact'] as String,
+        designation: staffJson['designation'] as int,
+        emailAddress: staffJson['email_address'] as String,
+        staffid: staffJson['staffid'] as String,
+        district: staffJson['district'] as String?,
+        assignedDistricts: assignedDistricts,
+      );
+    } catch (e) {
+      debugPrint('Error getting staff from cache: $e');
+      return null;
+    }
+  }
+
+  // Clear staff data from cache
+  static Future<void> clearStaff() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.remove(_staffKey);
+    } catch (e) {
+      debugPrint('Error clearing staff cache: $e');
+    }
+  }
+
+  // Check if staff data exists in cache
+  static Future<bool> hasCachedStaff() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey(_staffKey);
+  }
+}
